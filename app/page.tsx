e8c1c945d95b0a98674cdd396fa2647173425b53
EhsaNik <ehsanik19@gmail.com>
2025-06-04 01:51:34 +0330

    feat: integrate Tailwind CSS with PostCSS and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import "./globals.css";
+
 export default function Home() {
-  return "hello!"
+	return (
+		<div className='min-h-screen bg-gray-100 flex items-center justify-center'>
+			<h1 className='text-4xl font-bold text-blue-600'>
+				سلام! تیلویند نصب شد!
+			</h1>
+		</div>
+	);
 }
