f1720915d03a0b57db5ee256a400815d9a35947d
EhsaNik <ehsanik19@gmail.com>
2025-06-04 17:13:32 +0330

    feat: create welcome page with logo, animation, and navigation buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-import "./globals.css";
+'use client'
+import './globals.css'
+import Image from 'next/image'
+import Lottie from 'lottie-react'
+import { useRouter } from 'next/navigation'
+import logo from '../public/assets/logo.png'
+import welcomeAnimation from '../public/assets/welcome.json'
 
-export default function Home() {
-	return (
-		<div className='min-h-screen bg-gray-100 flex items-center justify-center'>
-			<h1 className='text-4xl font-bold text-blue-600'>
-				سلام! تیلویند نصب شد!
-			</h1>
-		</div>
-	);
+export default function Welcome() {
+  const router = useRouter()
+
+  const handleNavigate = (path: string) => {
+    router.push(path)
+  }
+
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-start gap-4 px-6">
+      <Image src={logo} alt="logo" width={62} height={51} className="mt-16" />
+      <Lottie animationData={welcomeAnimation} loop className="w-72 h-72" />
+      <h1 className="text-2xl font-bold">Welcome</h1>
+      <p className="text-sm font-normal text-center">
+        A brief, AI-powered summary of your personal and professional background, crafted from the
+        details you provide.
+      </p>
+      <button
+        onClick={() => handleNavigate('/signup')}
+        className="w-full text-sm font-bold text-white bg-black py-2.5 mt-16 rounded-3xl"
+      >
+        Sign Up
+      </button>
+      <button onClick={() => handleNavigate('/signin')} className="text-sm font-normal mt-2">
+        Or Sign In
+      </button>
+    </div>
+  )
 }
