24e166fc51dfea850c6f87a6a25907e4e7efafe3
EhsaNik <ehsanik19@gmail.com>
2025-06-05 19:27:49 +0330

    feat: replace Geist font with Gilroy font family in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,47 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import './globals.css'
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const gilroy = localFont({
+  src: [
+    {
+      path: '../public/fonts/Gilroy-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gilroy-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gilroy-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Gilroy-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-gilroy',
+})
 
 export const metadata: Metadata = {
-  title: "Long Bio",
-};
+  title: 'Long Bio',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={gilroy.variable}>
+        <section className="container max-w-[480px] mx-auto px-8">{children}</section>
       </body>
     </html>
-  );
+  )
 }
