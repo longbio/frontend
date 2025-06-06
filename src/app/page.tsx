fbae3a0868f6a6acc40206fb45c1db9179be2fbc
EhsaNik <ehsanik19@gmail.com>
2025-06-07 03:03:03 +0330

    feat: update Home component to Welcome with logo, animation, and sign-up links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return "hello!"
+import Link from 'next/link'
+import Logo from '@/src/components/Logo'
+import WelcomeAnimation from '@/src/components/WelcomeAnimation'
+
+export default function Welcome() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-start">
+      <Logo className="mt-16" />
+      <WelcomeAnimation />
+      <h1 className="text-2xl font-bold">Welcome</h1>
+      <p className="text-sm font-normal text-center mt-7">
+        A brief, AI-powered summary of your personal and professional background, crafted from the
+        details you provide.
+      </p>
+      <Link
+        href="/auth/signup"
+        className="w-full text-sm font-semibold text-white bg-black py-2.5 mt-24 rounded-3xl text-center"
+      >
+        Sign Up
+      </Link>
+      <Link href="/auth/signin" className="text-sm font-normal mt-4">
+        Or Sign In
+      </Link>
+    </div>
+  )
 }
