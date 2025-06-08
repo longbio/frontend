56d200dead0d58680629716c47711f155e46da88
EhsaNik <ehsanik19@gmail.com>
2025-06-09 00:11:09 +0330

    fix: use react form hook in verify page

--- src/app/auth/signup/verify/page.tsx
+++ src/app/auth/signup/verify/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import clsx from 'clsx'
 import Link from 'next/link'
+import Logo from '@/components/Logo'
 import { useState, useEffect } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
@@ -32,23 +33,21 @@ export default function SignUp() {
     }
   }, [timeLeft])
 
-  const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60)
-    const remainingSeconds = seconds % 60
-    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`
-  }
-
-  // Auto verify when correct code is entered
   useEffect(() => {
     if (verificationCode === '123456') {
-      // Mock verification code
       setIsVerified(true)
     } else {
       setIsVerified(false)
     }
   }, [verificationCode])
 
-  const handleVerify = (e: React.FormEvent) => {
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = seconds % 60
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     if (isVerified) {
       router.push('/step1')
@@ -59,8 +58,9 @@ export default function SignUp() {
     <div className="flex flex-col min-h-screen items-center justify-start">
       <div className="w-full max-w-md space-y-8 mt-20">
         <div className="text-left text-purple-blaze">
-          <h2 className="text-sm font-bold text-black">Let&apos;s Start with ...</h2>
-          <h3 className="mt-2 text-[10px] font-normal">
+          <Logo />
+          <h2 className="text-sm font-bold text-black mt-12">Let&apos;s Start with ...</h2>
+          <h3 className="mt-1.5 text-[10px] font-normal">
             already have an account?
             <Link href="/auth/login" className="hover:underline">
               Login
@@ -68,70 +68,70 @@ export default function SignUp() {
           </h3>
         </div>
 
-        <form onSubmit={handleVerify} className="mt-40">
+        <form onSubmit={handleSubmit} className="mt-40">
           <div className="space-y-6">
-            <label aria-disabled htmlFor="email" className="text-xl font-bold text-light-gray">
-              Email
-            </label>
-            <Input
-              disabled
-              type="email"
-              value={email}
-              className="w-full text-light-gray mt-6 disabled:border-light-gray"
-            />
-            <label htmlFor="text" className="text-xl font-bold">
-              Verification Code
-            </label>
-            <div className="relative">
+            <div>
+              <label htmlFor="email" className="text-xl font-bold text-light-gray">
+                Email
+              </label>
               <Input
-                type="text"
-                placeholder="Please check your inbox"
-                className={clsx('w-full mt-6', {
-                  'border-red-500 focus:border-red-500 focus-visible:ring-0':
-                    isTimeUp && !isVerified,
-                  'border-green-500 focus:border-green-500 focus-visible:ring-0': isVerified,
-                })}
-                value={verificationCode}
-                onChange={(e) => setVerificationCode(e.target.value)}
+                disabled
+                type="email"
+                value={email}
+                className="w-full text-light-gray mt-6 disabled:border-light-gray"
               />
-              {!isVerified ? (
-                <button
-                  className={clsx(
-                    'absolute right-8 top-1/2 -translate-y-1/2 text-sm flex items-center gap-1',
-                    {
-                      'text-red-500': isTimeUp,
-                      'text-light-gray': !isTimeUp,
-                    }
-                  )}
-                  onClick={(e) => {
-                    e.preventDefault()
-                    if (isTimeUp) {
-                      resetTimer()
-                    }
-                  }}
-                >
-                  {isTimeUp ? (
-                    <>
-                      <h1 className="text-sm font-light">Recent</h1>
-                      <RotateCcw className="size-4" />
-                    </>
-                  ) : (
-                    <>
-                      {formatTime(timeLeft)}
-                      <AlarmClock className="size-4" />
-                    </>
-                  )}
-                </button>
-              ) : (
-                <div className="absolute right-8 top-1/2 -translate-y-1/2 text-green-500">
-                  <CheckCircle2 className="size-5" />
-                </div>
-              )}
-              {isTimeUp && !isVerified && (
-                <p className="absolute text-red-500 text-xs mt-1">
-                  Please enter Verification code we sent to your email adress
-                </p>
-              )}
+            </div>
+            <div>
+              <label htmlFor="verificationCode" className="text-xl font-bold">
+                Verification Code
+              </label>
+              <div className="relative">
+                <Input
+                  type="text"
+                  placeholder="Please check your inbox"
+                  className={clsx('w-full mt-6', {
+                    'border-red-500 focus-visible:ring-red-500/50': isTimeUp && !isVerified,
+                    'border-green-500 focus-visible:ring-green-500/50': isVerified,
+                  })}
+                  value={verificationCode}
+                  onChange={(e) => setVerificationCode(e.target.value)}
+                />
+                {!isVerified ? (
+                  <button
+                    type="button"
+                    className={clsx(
+                      'absolute right-8 top-1/2 -translate-y-1/2 text-sm flex items-center gap-1',
+                      {
+                        'text-red-500': isTimeUp,
+                        'text-light-gray': !isTimeUp,
+                      }
+                    )}
+                    onClick={resetTimer}
+                    disabled={!isTimeUp}
+                  >
+                    {isTimeUp ? (
+                      <>
+                        <h1 className="text-sm font-light">Recent</h1>
+                        <RotateCcw className="size-4" />
+                      </>
+                    ) : (
+                      <>
+                        {formatTime(timeLeft)}
+                        <AlarmClock className="size-4" />
+                      </>
+                    )}
+                  </button>
+                ) : (
+                  <div className="absolute right-8 top-1/2 -translate-y-1/2 text-green-500">
+                    <CheckCircle2 className="size-5" />
+                  </div>
+                )}
+                {isTimeUp && !isVerified && (
+                  <p className="absolute text-red-500 text-xs mt-1">
+                    Please enter Verification code we sent to your email address
+                  </p>
+                )}
+              </div>
             </div>
           </div>
 
