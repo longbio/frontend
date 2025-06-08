1ad7c81d89f1bb52effa8f08aa6ef78ef411eb06
EhsaNik <ehsanik19@gmail.com>
2025-06-09 02:00:31 +0330

    fix: remove extra div in success page

--- src/app/auth/signin/success/page.tsx
+++ src/app/auth/signin/success/page.tsx
@@ -3,9 +3,7 @@ import Logo from '@/components/Logo'
 export default function Success() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="text-left mt-16">
-        <Logo />
-      </div>
+      <Logo className="text-left mt-16" />
       <span className="flex h-full text-center text-xl font-bold items-center justify-center">
         Welcome back <br />
         Farzaneh!
