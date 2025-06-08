42840592783c81aba73e8e7c107eba73d6dd3cf5
EhsaNik <ehsanik19@gmail.com>
2025-06-08 20:40:54 +0330

    fix: fix signup component to capture and pass email to verify page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
 export default function SignUp() {
   const router = useRouter()
+  const [email, setEmail] = useState('')
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    router.push('/auth/signup/verify')
+    const searchParams = new URLSearchParams()
+    searchParams.set('email', email)
+    router.push(`/auth/signup/verify?${searchParams.toString()}`)
   }
+
   return (
     <div className="flex flex-col min-h-screen items-center justify-start">
       <div className="w-full max-w-md space-y-8 mt-20">
         <div className="text-left text-purple-blaze">
-          <h2 className="text-sm font-bold text-black">Let’s Start with ...</h2>
+          <h2 className="text-sm font-bold text-black">Let&apos;s Start with ...</h2>
           <h3 className="mt-2 text-[10px] font-normal">
             already have an account?
             <Link href="/auth/login" className="hover:underline">
@@ -32,7 +38,13 @@ export default function SignUp() {
             <label htmlFor="email" className="text-xl font-bold">
               Email
             </label>
-            <Input type="email" placeholder="Exp: Fari@gmail.com" className="w-full mt-6" />
+            <Input
+              type="email"
+              placeholder="Exp: Fari@gmail.com"
+              className="w-full mt-6"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <Button
