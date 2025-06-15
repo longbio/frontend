b166e25da71ab0608fd9df8268583916f1ee6b13
EhsaNik <ehsanik19@gmail.com>
2025-06-15 18:22:48 +0330

    fix: remove chevron icon in dataPicker component

--- src/app/info/birthday/components/DatePicker.tsx
+++ src/app/info/birthday/components/DatePicker.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { useState } from 'react'
+import { format } from 'date-fns'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import { format } from 'date-fns'
-import { ChevronDownIcon } from 'lucide-react'
 
 interface DatePickerProps {
   onDateSelect: (date: Date) => void
@@ -30,7 +29,6 @@ export function DatePicker({ onDateSelect }: DatePickerProps) {
           className="w-full text-zinc-400 text-xs font-medium bg-cloud-mist border-none justify-center py-5.5 rounded-[100px]"
         >
           {date ? format(date, 'yyyy/MM/dd') : 'Exp: 1996/5/25'}
-          <ChevronDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
