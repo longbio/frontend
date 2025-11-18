e0c2d3faf4374d1bf3d4d0748c3f7b6b4036616d
Mohammad Norouzi <mm.74noroozi@gmail.com>
2025-11-18 12:15:12 +0300

    fix: update cookie key from 'id_token' to 'accessToken' in middleware for authentication

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('id_token')?.value
+  const token = request.cookies.get('accessToken')?.value
 
   if (!token && request.nextUrl.pathname.startsWith('/info')) {
     return NextResponse.redirect(new URL('/auth/signup', request.url))
