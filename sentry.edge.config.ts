826e41616631c452083849332d04e0cb9eb7b249
Mohammad Norouzi <mm.74noroozi@gmail.com>
2025-12-04 12:27:23 +0300

    feat: integrate Sentry error handling with custom error components for improved error tracking and user experience

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,6 +6,8 @@ Sentry.init({
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
 
+  sendDefaultPii: true,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 })
