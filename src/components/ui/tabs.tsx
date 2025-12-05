420ab4f44acc8b0dd7b32fa6264ca361e4429ca0
Mohammad Norouzi <mm.74noroozi@gmail.com>
2025-12-05 21:35:24 +0300

    refactor: increase tab height for improved UI consistency

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -13,10 +13,10 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn(
-      'inline-flex h-10 items-center justify-center rounded-full bg-gray-100 p-1 text-gray-500',
-      className
-    )}
+      className={cn(
+        'inline-flex h-12 items-center justify-center rounded-full bg-gray-100 p-1 text-gray-500',
+        className
+      )}
     {...props}
   />
 ))
