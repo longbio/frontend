4ca92e0a2b936e9a48d4f9945641432d9426e1c1
Mohammad Norouzi <mm.74noroozi@gmail.com>
2025-12-08 19:15:01 +0300

    feat: add TickButton component and update Header to conditionally render BackButton for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
 import clsx from 'clsx'
 import Logo from './Logo'
 import BackButton from './BackButton'
+import TickButton from './TickButton'
 import Link from 'next/link'
 
 type HeaderProps = {
   className?: string
   showTickButton?: boolean
+  showBackButton?: boolean
 }
 
-export default function Header({ className, showTickButton = false }: HeaderProps) {
+export default function Header({ className, showTickButton = false, showBackButton = false }: HeaderProps) {
   return (
     <header className={clsx('flex items-center justify-between', className)}>
       <Link href="/">
         <Logo />
       </Link>
-      {showTickButton && <BackButton />}
+      {showTickButton && <TickButton />}
+      {showBackButton && <BackButton />}
     </header>
   )
 }
