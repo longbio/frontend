1292027045e8090cb96b9a81b9527ad1b6ce948c
Mohammad Norouzi <mm.74noroozi@gmail.com>
2025-12-15 19:09:41 +0300

    feat: create Welcome page with header, animation, and sign-up/sign-in links

---
+++ src/app/fa/page.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import Header from '@/components/Header'
+import WelcomeAnimation from '@/app/components/WelcomeAnimation'
+
+export default function Welcome() {
+  return (
+    <div dir="rtl" className="flex flex-col items-center justify-between bg-white p-8 h-full w-full">
+      <div className="flex flex-col items-center">
+        <Header />
+        <WelcomeAnimation className="mt-14" />
+        <h1 className="text-2xl font-bold mt-5">خوش آمدید</h1>
+        <p className="text-sm font-normal text-center mt-4">
+          پروفایل لانگ‌بیو خود را در چند دقیقه بسازید. آن را به اشتراک بگذارید تا دوستان و جامعه‌تان شما را بهتر بشناسند.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full">
+        <Link
+          href="/auth/signup"
+          className="w-full text-sm font-semibold text-white bg-black hover:bg-gray-800 py-3.5 rounded-3xl text-center"
+        >
+          ثبت نام
+        </Link>
+        <Link href="/auth/signin" className="text-sm font-normal p-3.5">
+          یا وارد شوید
+        </Link>
+      </div>
+      <div className="mt-8 text-center">
+        <span className="text-sm text-gray-600">
+          با تیم ما در تماس باشید{' '}
+          <a
+            href="https://wa.me/971555345004"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-600 hover:text-gray-800 underline"
+          >
+            اینجا
+          </a>
+        </span>
+      </div>
+    </div>
+  )
+}
+
